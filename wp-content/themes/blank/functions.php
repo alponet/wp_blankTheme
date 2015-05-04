3a9ccf5d7212852b0d54bd85d454681bb70f2e7a
Thomas <thomas@wigwam.im>
2015-05-04 15:30:49 +0200

    basic theme files

---
+++ wp-content/themes/blank/functions.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Theme functions and definitions
+ *
+ */
+
+show_admin_bar(false);
+
+function theme_add_scripts() {
+	wp_enqueue_script( 'jquery', get_template_directory_uri() . '/js/jquery-2.1.3.min.js');
+	wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js');
+}
+add_action( 'wp_enqueue_scripts', 'theme_add_scripts' );
+
+function theme_add_styles() {
+	wp_enqueue_style( 'bootstrap-css', get_template_directory_uri() . '/css/bootstrap.min.css' );
+	wp_enqueue_style( 'wigwam-css', get_template_directory_uri() . '/style.css' );
+}
+add_action( 'wp_enqueue_scripts', 'theme_add_styles' );
---
+++ wp-content/themes/blank/index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * The main template file.
+ */
+
+get_header(); ?>
+
+<div class="container">
+NO CONTENT
+</div>
+
+<?php get_footer(); ?>
